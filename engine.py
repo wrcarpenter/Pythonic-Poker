59312e692d6aaa4dd0edaf57e259d388589f2b6f
Will Carpenter <65867547+wrcarpenter@users.noreply.github.com>
2024-06-22 20:18:23 -0400

    Add files via upload

--- engine.py
+++ engine.py
@@ -14,6 +14,13 @@ import random
 # class Rankings - hand rankings 
 # use tuples to create the deck
 
+card_categories = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+cards_list = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+deck = [(card, category) for category in card_categories for card in cards_list]
+
+newdeck = deck.random.shuffle()
+
+#%%
 
 class Deck:
     
@@ -23,34 +30,62 @@ class Deck:
 
     def __init__(self):
 
-        suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
-        typs  = ['Ace', 'Queen', 'King', 'Jack' '2', '3', '4', '5', '6', '7', '8', '9', '10']
+        self.cards = self.create_deck()
+        self.size  = self.deck_size()
 
-
-        self.cards = [(card, category) for category in suits for card in typs] 
-        self.size  = len(self.cards)
-
-
-    def size(self):
-        return self.size    
+    def create_deck(self):
+        # create the initial version of a deck
+        
+        card_categories = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+        
+        cards_list = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+        
+        deck = [(card, category) for category in card_categories for card in cards_list]
+        
+        return deck
+    
+    def __str__(self):
+        s = ""
+        for c in self.cards:
+            s += "%s\n" % c
+        return s    
+    
+    
+    def deck_size(self):
+        return len(self.cards)  # should return 52 cards unless deck is smaller
   
     def shuffle(self):
         # shuffle the deck
-        return random.shuffle(self.cards)
+        random.shuffle(self.cards)
     
+    def card(self):
+        return self.cards.pop() 
+        
     def hand(self):
         # deal a two-card hand to a player
         pass    
 
-    def card(self):
-        # pop a card from the top of the deck
-        pass
-
-
 def main():
+    
+    # creates a fresh deck of 52 cards, not shuffled 
     game_deck = Deck()
-    game_deck = game_deck.shuffle()
-    print(type(game_deck))
+    
+    print(game_deck.cards)
+    print("   ")
+    
+    print(type(game_deck.cards))
+    
+    print(game_deck.deck_size())
+    print("   ")
+    
+    game_deck.shuffle()
+    
+    print(game_deck.cards)
+    
+    card = game_deck.card()
+    print("   ")
+    print(card)
+    
 
 if __name__ == "__main__":
 
